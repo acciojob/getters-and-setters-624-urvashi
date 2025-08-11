38e840000cd45a72ed1bc1015381af10fd697c61
624-urvashi <143706170+624-urvashi@users.noreply.github.com>
2025-08-11 21:21:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
-//complete this code
-class Person {}
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    
+    get getName() {
+        return this.name;
+    }
 
-class Student extends Person {}
+    set setAge(newAge) {
+        this.age = newAge; // Correctly set the new age
+    }
+}
 
-class Teacher extends Person {}
+const person = new Person("John", 30);
+person.setAge = 25; // Using the setter to change the age
+console.log(person.name, person.age); // Outputs: John 25
+
+class Student extends Person {
+    constructor(name, age) {
+        super(name, age); // Call the parent constructor
+    }
+    
+    study() {
+        console.log(`${this.name} is studying.`);
+    }
+}
+
+const student = new Student("Alice", 20);
+student.study(); // Outputs: Alice is studying.
+
+class Teacher extends Person { // Added missing braces
+    constructor(name, age) {
+        super(name, age);
+    }
+   
+    teach() {
+        console.log(`${this.name} is teaching.`);
+    }
+}
+
+const teacher = new Teacher("Bob", 40);
+teacher.teach(); // Outputs: Bob is teaching.
 
 // Do not change the code below this line
 window.Person = Person;
